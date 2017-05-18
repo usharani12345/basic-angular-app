53290b04d5b934992e23e1700aa9a3a5d49d9be9
usharani12345 <ulangalausharani@gmail.com>
2017-05-18 10:46:40 +0530

    this is my third commit

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,34 @@
-var app= angular.module("myApp",[]);
-app.controller("myController",["$scope",function($scope){
-	$scope.message="Hello World";
-	$scope.username ="john";
-	$scope.password="12345";
+var app = angular.module("myApp",['ngRoute']);
+app.config(["$locationProvider",function($locationProvider){
+	$locationProvider.hashPrefix('');
+}])
+app.config(['$routeProvider',function($routeProvider){
+	$routeProvider
+	.when("/",{
+		templateUrl:"index.html",
+		controller:"myHomeController"
+	})
+	.when("/register",{
+		templateUrl:"Register.html",
+		controller:"myRegisterController"
+	})
+	.when("/login",{
+		templateUrl:"Login.html",
+		controller:"myLoginController"
+	})
+}])
+
+app.run([function(){
+	
+}])
+
+app.controller("myHomeController",["$scope",function($scope){
+	$scope.message1="inside Home controller";
+}])
+
+app.controller("myRegisterController",["$scope",function($scope){
+	$scope.name="usha";
+}])
+app.controller("myLoginController",["$scope",function($scope){
+	$scope.message3="inside login controller";
 }])
